eda4cca482bc241acb51f70fea10f3e72489b378
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-07-11 11:03:51 +0800

    Create draw.cc

---
+++ draw.cc
@@ -0,0 +1,22 @@
+
+line(src, Point(x1,y2), Point(x2, y2), Scalar(r,g,b),2);
+
+circle(src, Point(x,y), 2, Scalar(r,g,b), -1);
+
+rectangle(src, Point(x1,y1), Point(x2,y2), Scalar(r,g,b), 4, 8, 0);
+
+//=== Poly line ===
+const Point *pts = (const cv::Point*) Mat(contour).data;
+const Point *ppt[1] = {&points_out[0]};
+int npt[] = { 4 }; //number of vertex
+polylines(  src, ppt, npt,
+          1, //closed
+          1, //linewidth=1
+          Scalar( r, g, b ),
+          2);     //lineType=8
+}
+
+//=== Display ===
+namedWindow( "display_window", WINDOW_AUTOSIZE );
+imshow( "display_window", src);
+waitKey();// display window stay until key pressed
