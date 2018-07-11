cdfcd990380a9f7a348f7f66447a2d975f1d5588
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-07-11 11:20:05 +0800

    Create geometry.cc

---
+++ geometry.cc
@@ -0,0 +1,21 @@
+//=== Convert Point vector to Mat===
+vector<Point2f> pt_vec;
+Mat pt_mat = Mat(pt_vec).clone();
+
+//==== Convert Mat to vector ===
+Point *pts = (const Point*) Mat(contour).data;
+
+//=== Warp image   ====
+warpPerspective ( src, dst, T, Size(w,h), INTER_LINEAR, BORDER_CONSTANT);
+
+//=== Points Transformation
+perspectiveTransform(p1, p2, T);//for points!
+
+//=== Calculate distantce
+double cost = norm(Mat(p1), Mat(p2), NORM_L2);
+
+//=== Homography matrix estimation ====
+findHomography( dst_pt, src_pt,RANSAC );
+
+//=== Perspective matrix estimation ====
+getPerspectiveTransform(const Point2f src[], const Point2f dst[])
