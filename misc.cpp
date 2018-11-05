daa6ba0b0c798ff4e8278872e3709a4c79e8244e
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-11-05 13:13:18 +0800

    Update misc.cpp

--- misc.cpp
+++ misc.cpp
@@ -1,4 +1,4 @@
 int64 pre_timer, cur_timer;
 pre_timer = cur_timer; 
 cur_timer = cv::getTickCount();
-cout<<"Time spent "<<1000000000/(cur_timer-pre_timer)<<" Seconds  "<<endl;  
+cout<<"Time spent "<<(cur_timer-pre_timer)/ cv::getTickFrequency()<<" Seconds  "<<endl;  
