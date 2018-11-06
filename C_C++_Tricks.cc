74d931bd89b16cf73e9104c86d22b46560129455
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-11-06 10:22:38 +0800

    Update C_C++_Tricks.cc

--- C_C++_Tricks.cc
+++ C_C++_Tricks.cc
@@ -6,3 +6,10 @@ sprintf(str, "%d", t);
 ostringstream fn;
 fn<<"./output/xxx_"<<k<<".JPG";
 (fn.str()).c_str()
+  
+//=====Time Performance=====
+struct timespec start, end;
+clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+//------------------
+clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+uint64_t delta_us = (end.tv_nsec - start.tv_nsec) / 1000;
