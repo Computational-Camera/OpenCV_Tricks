9cc494ce9d2c5854be6b8f73cf486088de67e05c
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2018-12-18 20:38:42 +0800

    Create Classes.cc

---
+++ Classes.cc
@@ -0,0 +1,17 @@
+Point2i p(x,y); //2D point integer
+Point3f p(x,y,x); //3D point floating point
+
+Rect r = Rect(x,y,w,h); //or Rect(p1,p2);
+r.tl()
+r.br()
+
+Matx33f m; //3 by 3 matrix
+Vec3f(x0,x1,x2) //vector
+
+fastMalloc();//loate memory aligned go a 16 byte boundary
+
+setUseOptimized(1);//set 1 to enable
+flag = useOptimized();//return 1 means optmized
+
+
+
