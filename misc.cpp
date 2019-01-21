3e2a633dcd9e11bb1184430c6ae696932f763196
Shan Xu <1936800+Computational-Camera@users.noreply.github.com>
2019-01-21 16:43:13 +0800

    Update misc.cpp

--- misc.cpp
+++ misc.cpp
@@ -2,3 +2,9 @@ int64 pre_timer, cur_timer;
 pre_timer = cur_timer; 
 cur_timer = cv::getTickCount();
 cout<<"Time spent "<<(cur_timer-pre_timer)/ cv::getTickFrequency()<<" Seconds  "<<endl;  
+
+//random number
+#include <time.h>
+srand((unsigned)time(NULL));
+float jitter = float(rand()%100)/100 -0.5 ; //RAND_MAX is a constant defined in <cstdlib>. -0.5 to 0.5
+
